f4653c9711e4feee23f1c53040ab2e1bb988782b
Marc Friedmann <42418622+Plata101@users.noreply.github.com>
2019-11-28 13:39:31 +0100

    Add files via upload

---
+++ adjust.js
@@ -0,0 +1,8 @@
+var data = [{
+"tagline": "*99% Sale:* Schweiz ALLES unlimitiert & unlimitiertes Surfen in EU. *Jetzt nur CHF 37.50/Mt.*",
+"cta": "zum Angebot",
+"bild_pfad": "CDBANNDE.png",
+"datum": "July 27, 2019 22:00:00",
+"offer_valid": "27. Juli 2019",
+"url": "https://www.sunrise.ch",
+}]
